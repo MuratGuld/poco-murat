cb49acd43c9073717a3f8ef6f5bd6e4e7956ab0f
murat <slimplus1952@gmail.com>
2022-10-18 13:21:55 +0200

    I created a recipe card and display it on console

---
+++ week5/Recipe_Card/script.js
@@ -0,0 +1,11 @@
+let recipe = {
+  title: "Soup",
+  servings: 2,
+  ingredients: ["cinnamon", "cumin", "cocoa"],
+};
+
+console.log(`
+${recipe.title}
+Serves: ${recipe.servings}
+Ingredients: ${recipe.ingredients.toString()}
+`);
