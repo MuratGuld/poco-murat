3301d8c3e0d87d92976b0064affbb54e3bfbaa03
murat <slimplus1952@gmail.com>
2022-10-18 11:13:44 +0200

    I exercised with array and objects

---
+++ week5/Array_Object_Exercises/script.js
@@ -0,0 +1,23 @@
+// INSTANSE OF OBJECT
+function Person(name = "User", age = 18, married = false) {
+  this.name = name;
+  this.age = age;
+  this.married = married;
+
+  this.introduce = function () {
+    console.log(`My name is ${name}. I am ${age} years old.`);
+  };
+}
+
+let murat = new Person("Murat", 32, true);
+// console.log(Object.keys(murat));
+// console.log(Object.values(murat));
+
+// ARRAYS
+let firstArray = ["HTML", "CSS", "SCSS", "Bootstrap"];
+let secondArray = ["JS", "PHP"];
+
+let combinedArray = firstArray.concat(secondArray);
+combinedArray.push("Java");
+combinedArray.pop();
+
