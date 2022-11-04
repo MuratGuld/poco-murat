67abce04c96ca4ad4a6d90c6a7d9520756aa6ace
MuratGuld <slimplus1952@gmail.com>
2022-11-04 13:21:36 +0100

    I used a weather API to get current weather of cities

---
+++ week7/authors/scripts.js
@@ -0,0 +1,77 @@
+let authors = [
+  {
+    firstName: "Andreas",
+    lastName: "Neeser",
+  },
+  {
+    firstName: "Anna",
+    lastName: "Ruchat",
+  },
+  {
+    firstName: "Arno",
+    lastName: "Camenisch",
+  },
+  {
+    firstName: "Barbara",
+    lastName: "Schibli",
+  },
+  {
+    firstName: "Demian",
+    lastName: "Leinhard",
+  },
+  {
+    firstName: "Flurina",
+    lastName: "Bader",
+  },
+  {
+    firstName: "Franco",
+    lastName: "Supino",
+  },
+  {
+    firstName: "Lukas",
+    lastName: "Hartmann",
+  },
+  {
+    firstName: "Marius",
+    lastName: "Popescu",
+  },
+  {
+    firstName: "Reto",
+    lastName: "Haenny",
+  },
+  {
+    firstName: "Sandra",
+    lastName: "Kuenzi",
+  },
+  {
+    firstName: "Simon",
+    lastName: "Libsig",
+  },
+];
+
+function fonk(e) {
+  e.nextElementSibling.firstChild.style.color = "red";
+}
+
+let ul = document.querySelector("main ul");
+
+authors.forEach((author, index) => {
+  let li = document.createElement("li");
+  let img = document.createElement("img");
+  img.setAttribute("onclick", "fonk(this)");
+  img.setAttribute("id", `image-${index}`);
+  img.setAttribute(
+    "src",
+    `img/tile_${author.firstName}_${author.lastName}.jpg`
+  );
+
+  let h3 = document.createElement("h3");
+  let span = document.createElement("span");
+  let text = document.createTextNode(`${author.firstName} ${author.lastName}`);
+  span.appendChild(text);
+  h3.appendChild(span);
+
+  li.appendChild(img);
+  li.appendChild(h3);
+  ul.appendChild(li);
+});
